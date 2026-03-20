413a8a5e514a48f391b519dc0ef55ae593387212
ngMigthy.Astro <cesarjaramillormz@gmail.com>
2026-03-20 15:55:33 -0600

    refactor: eliminar configuración de sitio y base en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://ngMigthy-astro.github.io",
-  base: "/portfolio-migthy",
   vite: {
     plugins: [tailwindcss()],
   },
